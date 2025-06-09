5ce7401498e789db3a2e1ce571de7095d311a251
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-06-09 16:03:25 +0200

    Update Icon.tsx

--- src/components/seldon/primitives/Icon.tsx
+++ src/components/seldon/primitives/Icon.tsx
@@ -4,7 +4,11 @@
  * Do not redistribute or sublicense without permission.
  */
 import { IconDefault } from "../icons/IconDefault";
+import { IconMaterialAdd } from "../icons/IconMaterialAdd";
+import { IconMaterialFavorite } from "../icons/IconMaterialFavorite";
+import { IconMaterialFullscreen } from "../icons/IconMaterialFullscreen";
 import { IconMaterialPerson } from "../icons/IconMaterialPerson";
+import { IconMaterialUpload } from "../icons/IconMaterialUpload";
 import { CSSProperties } from "react";
 import React, { SVGAttributes } from "react";
 
@@ -12,7 +16,13 @@ interface IconTokens {}
 
 type IconProps = SVGAttributes<SVGSVGElement> &
   IconTokens & {
-    icon: "__default__" | "material-person";
+    icon:
+      | "__default__"
+      | "material-favorite"
+      | "material-upload"
+      | "material-add"
+      | "material-fullscreen"
+      | "material-person";
   };
 
 export function Icon({ icon = "__default__", style, ...props }: IconProps) {
@@ -29,5 +39,9 @@ const defaultStyles: CSSProperties = {
 
 const iconMap = {
   __default__: IconDefault,
+  "material-favorite": IconMaterialFavorite,
+  "material-upload": IconMaterialUpload,
+  "material-add": IconMaterialAdd,
+  "material-fullscreen": IconMaterialFullscreen,
   "material-person": IconMaterialPerson,
 };
