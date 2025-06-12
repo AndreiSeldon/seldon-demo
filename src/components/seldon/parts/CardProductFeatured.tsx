48f8e91c319838b08863660a791ff7063d03371b
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-06-12 15:06:45 +0200

    Update CardProductFeatured.tsx

--- src/components/seldon/parts/CardProductFeatured.tsx
+++ src/components/seldon/parts/CardProductFeatured.tsx
@@ -3,30 +3,52 @@
  * Licensed under the Terms of Use: https://seldon.app/terms
  * Do not redistribute or sublicense without permission.
  */
-import { CSSProperties } from "react";
-import { HTMLAttributes } from "react";
-import { Description } from "../primitives/Description";
-import { Icon } from "../primitives/Icon";
-import { LabelButton } from "../primitives/LabelButton";
-import { Title } from "../primitives/Title";
-import { Frame } from "../frames/Frame";
-import { Button } from "../elements/Button";
-import { ButtonBar } from "../elements/ButtonBar";
-import { TextblockDetails } from "../elements/TextblockDetails";
+import { CSSProperties, HTMLAttributes } from "react"
+import { DescriptionProps } from "../primitives/Description"
+import { IconProps } from "../primitives/Icon"
+import { LabelButtonProps } from "../primitives/LabelButton"
+import { TitleProps } from "../primitives/Title"
+import { Frame } from "../frames/Frame"
+import { ButtonBarProps, ButtonBar } from "../elements/ButtonBar"
+import { ButtonChipProps } from "../elements/ButtonChip"
+import {
+  TextblockDetailsProps,
+  TextblockDetails,
+} from "../elements/TextblockDetails"
 
-interface CardProductFeaturedTokens {}
-
-type CardProductFeaturedProps = HTMLAttributes<HTMLElement> &
-  CardProductFeaturedTokens;
+export type CardProductFeaturedProps = HTMLAttributes<HTMLElement> & {
+  textblockDetailsProps?: TextblockDetailsProps
+  textblockDetailsTitleProps?: TitleProps
+  textblockDetailsDescriptionProps?: DescriptionProps
+  textblockDetailsDescription2Props?: DescriptionProps
+  buttonBarProps?: ButtonBarProps
+  buttonBarButtonChipProps?: ButtonChipProps
+  buttonBarButtonChipIconProps?: IconProps
+  buttonBarButtonChipLabelButtonProps?: LabelButtonProps
+  buttonBarButtonChip2Props?: ButtonChipProps
+  buttonBarButtonChip2IconProps?: IconProps
+  buttonBarButtonChip2LabelButtonProps?: LabelButtonProps
+}
 
 export const CardProductFeatured = ({
   style,
+  textblockDetailsProps,
+  textblockDetailsTitleProps,
+  textblockDetailsDescriptionProps,
+  textblockDetailsDescription2Props,
+  buttonBarProps,
+  buttonBarButtonChipProps,
+  buttonBarButtonChipIconProps,
+  buttonBarButtonChipLabelButtonProps,
+  buttonBarButtonChip2Props,
+  buttonBarButtonChip2IconProps,
+  buttonBarButtonChip2LabelButtonProps,
   ...props
 }: CardProductFeaturedProps) => {
-  const styles = style || defaultStyles;
+  const styles = style || defaultStyles
 
   return (
-    <Frame style={styles} {...props}>
+    <Frame style={styles} {...{ ...defaultProps.component, ...props }}>
       <TextblockDetails
         style={{
           cursor: "pointer",
@@ -37,69 +59,27 @@ export const CardProductFeatured = ({
           flexDirection: "column",
           alignItems: "start",
           justifyContent: "start",
-          gap: "0.25rem",
+          gap: "0.5rem",
           alignSelf: "stretch",
           flex: "1 0 0",
         }}
-      >
-        <Title
-          style={{
-            color: "hsl(0deg 4% 98%)",
-            textShadow: "0px 1px 0.125rem hsl(0deg 4% 8% / 33%)",
-            alignSelf: "stretch",
-            height: "fit-content",
-            fontFamily: "Lexend",
-            fontStyle: "normal",
-            fontSynthesisStyle: "none",
-            fontWeight: 600,
-            fontSize: "2.002rem",
-            lineHeight: 1.25,
-            whiteSpace: "normal",
-            overflow: "hidden",
-            display: "-webkit-box",
-            WebkitLineClamp: 2,
-            lineClamp: 2,
-            WebkitBoxOrient: "vertical",
-            boxOrient: "vertical",
-          }}
-        >
-          Domplein 2
-        </Title>
-        <Description
-          style={{
-            color: "hsl(0deg 4% 98%)",
-            textShadow: "0px 1px 0.125rem hsl(0deg 4% 8% / 33%)",
-            alignSelf: "stretch",
-            height: "fit-content",
-            fontFamily: "Manrope",
-            fontStyle: "normal",
-            fontSynthesisStyle: "none",
-            fontWeight: 600,
-            fontSize: "0.875rem",
-            lineHeight: 1.25,
-            whiteSpace: "normal",
-          }}
-        >
-          3512 JE Utrecht
-        </Description>
-        <Description
-          style={{
-            color: "hsl(0deg 4% 98%)",
-            textShadow: "0px 1px 0.125rem hsl(0deg 4% 8% / 33%)",
-            alignSelf: "stretch",
-            height: "fit-content",
-            fontFamily: "Manrope",
-            fontStyle: "normal",
-            fontSynthesisStyle: "none",
-            fontWeight: 600,
-            fontSize: "0.875rem",
-            lineHeight: 1.25,
-            whiteSpace: "normal",
-          }}
-        >
-          €490.000
-        </Description>
-      </TextblockDetails>
+        {...{
+          ...defaultProps.children.textblockDetailsProps,
+          ...textblockDetailsProps,
+        }}
+        titleProps={{
+          ...defaultProps.children.textblockDetailsTitleProps,
+          ...textblockDetailsTitleProps,
+        }}
+        descriptionProps={{
+          ...defaultProps.children.textblockDetailsDescriptionProps,
+          ...textblockDetailsDescriptionProps,
+        }}
+        description2Props={{
+          ...defaultProps.children.textblockDetailsDescription2Props,
+          ...textblockDetailsDescription2Props,
+        }}
+      ></TextblockDetails>
       <ButtonBar
         style={{
           flexWrap: "wrap",
@@ -113,98 +93,69 @@ export const CardProductFeatured = ({
           width: "fit-content",
           height: "fit-content",
         }}
-      >
-        <Button
-          style={{
-            backgroundColor: "hsl(0deg 4% 8% / 50%)",
-            cursor: "pointer",
-            borderTopRightRadius: "0.5rem",
-            borderBottomRightRadius: "0.5rem",
-            borderBottomLeftRadius: "0.5rem",
-            borderTopLeftRadius: "0.5rem",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "0.5rem",
-            paddingTop: "0.5rem",
-            paddingRight: "0.875rem",
-            paddingBottom: "0.5rem",
-            paddingLeft: "0.75rem",
-            width: "fit-content",
-            height: "fit-content",
-          }}
-        >
-          <Icon
-            style={{ color: "hsl(0deg 4% 98%)", fontSize: "0.8rem" }}
-            icon="material-person"
-          />
-          <LabelButton
-            style={{
-              color: "hsl(0deg 4% 98%)",
-              fontFamily: "Manrope",
-              fontStyle: "normal",
-              fontSynthesisStyle: "none",
-              fontWeight: 500,
-              fontSize: "0.8rem",
-              lineHeight: 1.15,
-              letterSpacing: "0.1px",
-              whiteSpace: "nowrap",
-              textOverflow: "ellipsis",
-              overflow: "hidden",
-            }}
-          >
-            4
-          </LabelButton>
-        </Button>
-        <Button
-          style={{
-            backgroundColor: "hsl(0deg 4% 8% / 50%)",
-            cursor: "pointer",
-            borderTopRightRadius: "0.5rem",
-            borderBottomRightRadius: "0.5rem",
-            borderBottomLeftRadius: "0.5rem",
-            borderTopLeftRadius: "0.5rem",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "0.5rem",
-            paddingTop: "0.5rem",
-            paddingRight: "0.875rem",
-            paddingBottom: "0.5rem",
-            paddingLeft: "0.75rem",
-            width: "fit-content",
-            height: "fit-content",
-          }}
-        >
-          <Icon
-            style={{ color: "hsl(0deg 4% 98%)", fontSize: "0.8rem" }}
-            icon="material-fullscreen"
-          />
-          <LabelButton
-            style={{
-              color: "hsl(0deg 4% 98%)",
-              fontFamily: "Manrope",
-              fontStyle: "normal",
-              fontSynthesisStyle: "none",
-              fontWeight: 500,
-              fontSize: "0.8rem",
-              lineHeight: 1.15,
-              letterSpacing: "0.1px",
-              whiteSpace: "nowrap",
-              textOverflow: "ellipsis",
-              overflow: "hidden",
-            }}
-          >
-            150 m²
-          </LabelButton>
-        </Button>
-      </ButtonBar>
+        {...{ ...defaultProps.children.buttonBarProps, ...buttonBarProps }}
+        buttonChipProps={{
+          ...defaultProps.children.buttonBarButtonChipProps,
+          ...buttonBarButtonChipProps,
+        }}
+        buttonChipIconProps={{
+          ...defaultProps.children.buttonBarButtonChipIconProps,
+          ...buttonBarButtonChipIconProps,
+        }}
+        buttonChipLabelButtonProps={{
+          ...defaultProps.children.buttonBarButtonChipLabelButtonProps,
+          ...buttonBarButtonChipLabelButtonProps,
+        }}
+        buttonChip2Props={{
+          ...defaultProps.children.buttonBarButtonChip2Props,
+          ...buttonBarButtonChip2Props,
+        }}
+        buttonChip2IconProps={{
+          ...defaultProps.children.buttonBarButtonChip2IconProps,
+          ...buttonBarButtonChip2IconProps,
+        }}
+        buttonChip2LabelButtonProps={{
+          ...defaultProps.children.buttonBarButtonChip2LabelButtonProps,
+          ...buttonBarButtonChip2LabelButtonProps,
+        }}
+      ></ButtonBar>
     </Frame>
-  );
-};
-
+  )
+}
+type DefaultProps = {
+  component: CardProductFeaturedProps
+  children: CardProductFeaturedProps
+}
+const defaultProps: DefaultProps = {
+  component: {},
+  children: {
+    textblockDetailsProps: {},
+    textblockDetailsTitleProps: {
+      children: "Domplein 2",
+    },
+    textblockDetailsDescriptionProps: {
+      children: "3512 JE Utrecht",
+    },
+    textblockDetailsDescription2Props: {
+      children: "€490.000",
+    },
+    buttonBarProps: {},
+    buttonBarButtonChipProps: {},
+    buttonBarButtonChipIconProps: {
+      icon: "material-person",
+    },
+    buttonBarButtonChipLabelButtonProps: {
+      children: "4",
+    },
+    buttonBarButtonChip2Props: {},
+    buttonBarButtonChip2IconProps: {
+      icon: "material-fullscreen",
+    },
+    buttonBarButtonChip2LabelButtonProps: {
+      children: "150 m²",
+    },
+  },
+}
 const defaultStyles: CSSProperties = {
   backgroundColor: "hsl(0deg 4% 98%)",
   backgroundImage:
@@ -229,4 +180,4 @@ const defaultStyles: CSSProperties = {
   boxShadow: "0px 4px 0.375rem 0.125rem hsl(0deg 4% 8% / 15%)",
   alignSelf: "stretch",
   height: "24rem",
-};
+}
