285da4b49ea5877964aa35d81f05c1311563f8e8
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-07-01 16:14:48 +0200

    Update ListingTextAdd.tsx

---
+++ src/components/seldon/elements/ListingTextAdd.tsx
@@ -0,0 +1,121 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { HTMLLi } from "../native-react/HTML.Li"
+import { CSSProperties, HTMLAttributes } from "react"
+import { IconProps } from "../primitives/Icon"
+import { TextProps } from "../primitives/Text"
+import { Frame, FrameProps } from "../frames/Frame"
+import {
+  ButtonIconicAction,
+  ButtonIconicActionProps,
+} from "../elements/ButtonIconicAction"
+
+export interface ListingTextAddProps extends HTMLAttributes<HTMLLIElement> {
+  buttonIconicActionProps?: ButtonIconicActionProps
+  buttonIconicActionIconProps?: IconProps
+  frameProps?: FrameProps
+  frameTextProps?: TextProps
+}
+
+export function ListingTextAdd({
+  style,
+  buttonIconicActionProps,
+  buttonIconicActionIconProps,
+  frameProps,
+  frameTextProps,
+  ...props
+}: ListingTextAddProps) {
+  const styles = { ...defaultStyles, ...style }
+
+  return (
+    <HTMLLi style={styles} {...props}>
+      <ButtonIconicAction
+        {...{
+          ...defaultProps.buttonIconicActionProps,
+          ...buttonIconicActionProps,
+        }}
+        iconProps={{
+          ...defaultProps.buttonIconicActionIconProps,
+          ...buttonIconicActionIconProps,
+        }}
+      />
+      <Frame
+        {...{ ...defaultProps.frameProps, ...frameProps }}
+        textProps={{ ...defaultProps.frameTextProps, ...frameTextProps }}
+      />
+    </HTMLLi>
+  )
+}
+
+const defaultProps: ListingTextAddProps = {
+  buttonIconicActionProps: {
+    style: {
+      cursor: "pointer",
+      borderTopRightRadius: "99999px",
+      borderBottomRightRadius: "99999px",
+      borderBottomLeftRadius: "99999px",
+      borderTopLeftRadius: "99999px",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+      paddingTop: "0.5rem",
+      paddingRight: "0.5rem",
+      paddingBottom: "0.5rem",
+      paddingLeft: "0.5rem",
+      width: "fit-content",
+      height: "fit-content",
+    },
+  },
+  buttonIconicActionIconProps: {
+    icon: "material-add",
+    style: {
+      color: "hsl(0deg 4% 98%)",
+      fontSize: "0.75rem",
+    },
+  },
+  frameProps: {
+    style: {
+      cursor: "pointer",
+      flexWrap: "nowrap",
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "start",
+      alignSelf: "stretch",
+      flex: "1 0 0",
+    },
+  },
+  frameTextProps: {
+    children: "Add Shadow",
+    htmlElement: "p",
+    style: {
+      color: "hsl(0deg 4% 98%)",
+      display: "flex",
+      flexDirection: "row",
+      alignSelf: "stretch",
+      height: "fit-content",
+      fontFamily: "IBM Plex Sans",
+      fontStyle: "normal",
+      fontSynthesisStyle: "none",
+      fontWeight: 400,
+      fontSize: "0.75rem",
+      lineHeight: 1.25,
+      whiteSpace: "normal",
+    },
+  },
+}
+
+const defaultStyles: CSSProperties = {
+  cursor: "pointer",
+  flexWrap: "nowrap",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "center",
+  alignSelf: "stretch",
+  flex: "1 0 0",
+}
