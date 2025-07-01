d98cdc9b4ffe77783b7ba92c91faf49287e40afe
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-07-01 15:58:08 +0200

    Update LabelButton.tsx

---
+++ src/components/seldon/primitives/LabelButton.tsx
@@ -0,0 +1,43 @@
+/**
+ * This code was generated using Seldon (https://seldon.app)
+ * Licensed under the Terms of Use: https://seldon.app/terms
+ * Do not redistribute or sublicense without permission.
+ */
+import { HTMLLabel } from "../native-react/HTML.Label"
+import { HTMLSpan } from "../native-react/HTML.Span"
+import { CSSProperties, HTMLAttributes } from "react"
+
+export interface LabelButtonProps
+  extends HTMLAttributes<HTMLLabelElement | HTMLSpanElement> {
+  children?: string
+  htmlElement?: "span" | "label"
+}
+
+export function LabelButton({
+  style,
+  htmlElement,
+  ...props
+}: LabelButtonProps) {
+  const styles = { ...defaultStyles, ...style }
+
+  switch (htmlElement) {
+    case "label":
+      return <HTMLLabel style={styles} {...props} />
+    default:
+      return <HTMLSpan style={styles} {...props} />
+  }
+}
+
+const defaultStyles: CSSProperties = {
+  color: "hsl(0deg 4% 98%)",
+  fontFamily: "IBM Plex Sans",
+  fontStyle: "normal",
+  fontSynthesisStyle: "none",
+  fontWeight: 500,
+  fontSize: "0.875rem",
+  lineHeight: 1.15,
+  letterSpacing: "0.1px",
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+}
