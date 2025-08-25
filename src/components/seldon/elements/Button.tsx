231b75d007f3b1164ca3cf1531cb950d7d0b709d
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-08-25 14:21:06 +0200

    Seldon update

---
+++ src/components/seldon/elements/Button.tsx
@@ -0,0 +1,50 @@
+/*****
+ *
+ * This code was generated using Seldon (https://seldon.app)
+ *
+ * Licensed under the Terms of Use: https://seldon.digital/terms-of-service
+ * Do not redistribute or sublicense without permission.
+ *
+ * You may not use this software, or any derivative works of it,
+ * in whole or in part, for the purposes of training, fine-tuning,
+ * or otherwise improving (directly or indirectly) any machine learning
+ * or artificial intelligence system.
+ * 
+ *****/
+ 
+import { ButtonHTMLAttributes } from "react"
+import { HTMLButton } from "../native-react/HTML.Button"
+import { Icon, IconProps } from "../primitives/Icon"
+import { Label, LabelProps } from "../primitives/Label"
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string
+
+  icon?: IconProps
+  label?: LabelProps
+}
+
+export function Button({ className = "", icon, label, ...props }: ButtonProps) {
+  return (
+    <HTMLButton className={"sdn-button " + className} {...props}>
+      <Icon
+        {...{ ...sdn.icon, ...icon }}
+        className={"sdn-icon-0FTVEz3x sdn-icon " + (icon?.className ?? "")}
+      />
+      <Label
+        {...{ ...sdn.label, ...label }}
+        className={"sdn-label-RM76hUEF sdn-label " + (label?.className ?? "")}
+      />
+    </HTMLButton>
+  )
+}
+
+const sdn: ButtonProps = {
+  icon: {
+    icon: "__default__",
+  },
+  label: {
+    children: "Label",
+    htmlElement: "label",
+  },
+}
