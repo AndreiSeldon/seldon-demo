81e782e2557723b30a8352f103a554517252f61b
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-08-29 12:25:59 +0200

    Seldon update

---
+++ src/components/seldon/modules/BarNavigationMobile.tsx
@@ -0,0 +1,183 @@
+/*****
+ *
+ * This code was generated using Seldon (https://seldon.app)
+ *
+ * Licensed under the Terms of Use: https://seldon.digital/terms-of-service
+ * Do not redistribute or sublicense without permission.
+ *
+ * You may not use this software, or any derivative works of it,
+ * in whole or in part, for the purposes of training, fine-tuning,
+ * or otherwise improving (directly or indirectly) any machine learning
+ * or artificial intelligence system.
+ * 
+ *****/
+ 
+import { HTMLAttributes } from "react"
+import { Button, ButtonProps } from "../elements/Button"
+import { Frame } from "../frames/Frame"
+import { Icon, IconProps } from "../primitives/Icon"
+
+export interface BarNavigationMobileProps extends HTMLAttributes<HTMLElement> {
+  className?: string
+  button?: ButtonProps
+  icon?: IconProps
+  button2?: ButtonProps
+  icon2?: IconProps
+  button3?: ButtonProps
+  icon3?: IconProps
+  button4?: ButtonProps
+  icon4?: IconProps
+  button5?: ButtonProps
+  icon5?: IconProps
+}
+
+/**
+ * Navigation Bar
+ *
+ * Level: Module
+ *
+ * Intent: Provides primary navigation controls for traversing sections or views.
+ *
+ * Tags: navigation, navbar, menu, UI, header, section, links, routing
+ *
+ * @example
+ * ```tsx
+ * <BarNavigationMobile
+ *   button={() => {}}
+ *   icon="material-star"
+ *   button1={() => {}}
+ *   button2={() => {}}
+ *   button3={() => {}}
+ *   button4={() => {}}
+ * />
+ * ```
+ */
+export function BarNavigationMobile({
+  className = "",
+  button = sdn.button,
+  icon,
+  button2 = sdn.button2,
+  icon2,
+  button3 = sdn.button3,
+  icon3,
+  button4 = sdn.button4,
+  icon4,
+  button5 = sdn.button5,
+  icon5,
+  ...props
+}: BarNavigationMobileProps) {
+  return (
+    <Frame
+      className={"sdn-barNavigation sdn-barNavigation-yAQPDoP0 " + className}
+      {...props}
+    >
+      <Button
+        {...{ ...sdn.button, ...button }}
+        className={
+          "sdn-button-G8vyYXTc" +
+          (button?.className ? " " + button.className : "")
+        }
+        icon={{
+          ...sdn.icon,
+          ...icon,
+          className:
+            "sdn-icon-quwG9edJ" + (icon?.className ? " " + icon.className : ""),
+        }}
+      />
+      <Button
+        {...{ ...sdn.button2, ...button2 }}
+        className={
+          "sdn-button-G8vyYXTc" +
+          (button2?.className ? " " + button2.className : "")
+        }
+        icon={{
+          ...sdn.icon2,
+          ...icon2,
+          className:
+            "sdn-icon-quwG9edJ" +
+            (icon2?.className ? " " + icon2.className : ""),
+        }}
+      />
+      <Button
+        {...{ ...sdn.button3, ...button3 }}
+        className={
+          "sdn-button-G8vyYXTc" +
+          (button3?.className ? " " + button3.className : "")
+        }
+        icon={{
+          ...sdn.icon3,
+          ...icon3,
+          className:
+            "sdn-icon-quwG9edJ" +
+            (icon3?.className ? " " + icon3.className : ""),
+        }}
+      />
+      <Button
+        {...{ ...sdn.button4, ...button4 }}
+        className={
+          "sdn-button-G8vyYXTc" +
+          (button4?.className ? " " + button4.className : "")
+        }
+        icon={{
+          ...sdn.icon4,
+          ...icon4,
+          className:
+            "sdn-icon-quwG9edJ" +
+            (icon4?.className ? " " + icon4.className : ""),
+        }}
+      />
+      <Button
+        {...{ ...sdn.button5, ...button5 }}
+        className={
+          "sdn-button-G8vyYXTc" +
+          (button5?.className ? " " + button5.className : "")
+        }
+        icon={{
+          ...sdn.icon5,
+          ...icon5,
+          className:
+            "sdn-icon-quwG9edJ" +
+            (icon5?.className ? " " + icon5.className : ""),
+        }}
+      />
+    </Frame>
+  )
+}
+
+const sdn: BarNavigationMobileProps = {
+  button: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon: {
+    icon: "seldon-component",
+    className: "sdn-icon-quwG9edJ",
+  },
+  button2: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon2: {
+    icon: "seldon-image",
+    className: "sdn-icon-quwG9edJ",
+  },
+  button3: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon3: {
+    icon: "material-accountCircle",
+    className: "sdn-icon-quwG9edJ",
+  },
+  button4: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon4: {
+    icon: "seldon-frameColumns",
+    className: "sdn-icon-quwG9edJ",
+  },
+  button5: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon5: {
+    icon: "seldon-input",
+    className: "sdn-icon-quwG9edJ",
+  },
+}
