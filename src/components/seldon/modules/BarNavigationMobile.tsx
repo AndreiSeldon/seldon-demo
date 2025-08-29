e539f4aba1e3df106efd017e42589a6f6fb2a60e
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-08-29 12:51:45 +0200

    Seldon update

--- src/components/seldon/modules/BarNavigationMobile.tsx
+++ src/components/seldon/modules/BarNavigationMobile.tsx
@@ -13,36 +13,86 @@
  *****/
  
 import { HTMLAttributes } from "react"
+import { ButtonNav, ButtonNavProps } from "../elements/ButtonNav"
 import { Frame } from "../frames/Frame"
+import { Icon, IconProps } from "../primitives/Icon"
 
 export interface BarNavigationMobileProps extends HTMLAttributes<HTMLElement> {
   className?: string
+  buttonNav?: ButtonNavProps
+  icon?: IconProps
+  buttonNav2?: ButtonNavProps
+  icon2?: IconProps
 }
 
 /**
  * Navigation Bar
- * 
+ *
  * Level: Module
- * 
+ *
  * Intent: Provides primary navigation controls for traversing sections or views.
- * 
+ *
  * Tags: navigation, navbar, menu, UI, header, section, links, routing
- * 
+ *
  * @example
  * ```tsx
  * <BarNavigationMobile
-
+ *   buttonNav={() => {}}
+ *   icon="material-star"
+ *   buttonNav1={() => {}}
  * />
  * ```
  */
 export function BarNavigationMobile({
   className = "",
+  buttonNav,
+  icon,
+  buttonNav2,
+  icon2,
   ...props
 }: BarNavigationMobileProps) {
   return (
     <Frame
-      className={"sdn-barNavigation sdn-barNavigation-yAQPDoP0 " + className}
+      className={"sdn-barNavigation sdn-barNavigation-3LOEMAIz " + className}
       {...props}
-    />
+    >
+      {buttonNav && (
+        <ButtonNav
+          {...{ ...sdn.buttonNav, ...buttonNav }}
+          className={
+            "sdn-button-G8vyYXTc" +
+            (buttonNav?.className ? " " + buttonNav.className : "")
+          }
+          icon={{ ...sdn.icon, ...icon }}
+        />
+      )}
+      {buttonNav2 && (
+        <ButtonNav
+          {...{ ...sdn.buttonNav2, ...buttonNav2 }}
+          className={
+            "sdn-button-G8vyYXTc" +
+            (buttonNav2?.className ? " " + buttonNav2.className : "")
+          }
+          icon={{ ...sdn.icon2, ...icon2 }}
+        />
+      )}
+    </Frame>
   )
+}
+
+const sdn: BarNavigationMobileProps = {
+  buttonNav: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon: {
+    icon: "seldon-component",
+    className: "sdn-icon-1ihZT8Ll",
+  },
+  buttonNav2: {
+    className: "sdn-button-G8vyYXTc",
+  },
+  icon2: {
+    icon: "seldon-component",
+    className: "sdn-icon-1ihZT8Ll",
+  },
 }
