aaf57a02df9845269f9c8d61ec91b71b7fddc600
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-09-07 14:05:48 +0200

    Seldon update

---
+++ src/test/seldon/icons/IconMaterialHome.tsx
@@ -0,0 +1,30 @@
+/*****
+ *
+ * This code was generated using Seldon (https://seldon.app)
+ *
+ * Licensed under the Terms of Use: https://seldon.digital/terms-of-service
+ * Do not redistribute or sublicense without permission.
+ *
+ * You may not use this software, or any derivative works of it,
+ * in whole or in part, for the purposes of training, fine-tuning,
+ * or otherwise improving (directly or indirectly) any machine learning
+ * or artificial intelligence system.
+ * 
+ *****/
+ 
+import { SVGAttributes } from "react"
+
+export function IconMaterialHome(props: SVGAttributes<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" />
+    </svg>
+  )
+}
--- src/test/seldon/primitives/Icon.tsx
+++ src/test/seldon/primitives/Icon.tsx
@@ -14,11 +14,33 @@
  
 import { SVGAttributes } from "react"
 import { IconDefault } from "../icons/IconDefault"
+import { IconMaterialAccountCircle } from "../icons/IconMaterialAccountCircle"
+import { IconMaterialAdd } from "../icons/IconMaterialAdd"
+import { IconMaterialFavorite } from "../icons/IconMaterialFavorite"
+import { IconMaterialHome } from "../icons/IconMaterialHome"
+import { IconMaterialRemove } from "../icons/IconMaterialRemove"
 import { IconMaterialSearch } from "../icons/IconMaterialSearch"
+import { IconMaterialSend } from "../icons/IconMaterialSend"
+import { IconMaterialSettings } from "../icons/IconMaterialSettings"
+import { IconSocialInstagram } from "../icons/IconSocialInstagram"
+import { IconSocialLinkedin } from "../icons/IconSocialLinkedin"
+import { IconSocialTwitter } from "../icons/IconSocialTwitter"
 
 export interface IconProps extends SVGAttributes<SVGElement> {
   className?: string
-  icon?: "__default__" | "material-search"
+  icon?:
+    | "__default__"
+    | "material-search"
+    | "material-send"
+    | "social-twitter"
+    | "social-linkedin"
+    | "social-instagram"
+    | "material-add"
+    | "material-remove"
+    | "material-home"
+    | "material-favorite"
+    | "material-accountCircle"
+    | "material-settings"
 }
 
 /**
@@ -52,4 +74,14 @@ export function Icon({
 const iconMap = {
   __default__: IconDefault,
   "material-search": IconMaterialSearch,
+  "material-send": IconMaterialSend,
+  "social-twitter": IconSocialTwitter,
+  "social-linkedin": IconSocialLinkedin,
+  "social-instagram": IconSocialInstagram,
+  "material-add": IconMaterialAdd,
+  "material-remove": IconMaterialRemove,
+  "material-home": IconMaterialHome,
+  "material-favorite": IconMaterialFavorite,
+  "material-accountCircle": IconMaterialAccountCircle,
+  "material-settings": IconMaterialSettings,
 }
