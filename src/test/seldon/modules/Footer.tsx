a139d5fdf004692131f749b831e4bd2bf8ff9da5
Andrei Herasimchuk <138060170+AndreiSeldon@users.noreply.github.com>
2025-09-07 14:40:23 +0200

    Seldon update

--- src/test/seldon/modules/Footer.tsx
+++ src/test/seldon/modules/Footer.tsx
@@ -373,7 +373,7 @@ const sdn: FooterProps = {
     className: "sdn-sectionNav-8odyDgjE",
   },
   link: {
-    children: "FIXED",
+    children: "About",
     className: "sdn-link-3S3IhaQz",
   },
   link2: {
